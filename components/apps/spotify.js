32e20348df605a49a1962c2880a7693d6c91d5e3
vcto_ <82398490+notvcto@users.noreply.github.com>
2024-09-18 11:15:49 -0400

    changes

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,15 +2,18 @@ import React from "react";
 
 export default function Spotify() {
   return (
-    <iframe
-      src="https://open.spotify.com/playlist/0Pict9HQbfIuVe05q2NiSO"
-      frameBorder="0"
-      title="Spotify"
-      className="h-full w-full bg-ub-cool-grey"
-    ></iframe>
+<iframe src="https://open.spotify.com/embed/playlist/0Pict9HQbfIuVe05q2NiSO?utm_source=generator&theme=0" 
+width="100%" 
+height="100%" 
+frameBorder="0" 
+allowfullscreen="" 
+allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+loading="lazy">
+</iframe>
   );
 }
 
 export const displaySpotify = () => {
   <Spotify> </Spotify>;
 };
+
