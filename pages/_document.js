568f9915c98b44f40d54a926f679147b6517e73f
notvcto <git@notvc.to>
2025-07-09 18:08:57 -0400

    Fix hydration error by moving script to _app.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,18 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <script
-          data-name="BMC-Widget"
-          data-cfasync="false"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-          data-id="vcto"
-          data-description="Support me on Buy me a coffee!"
-          data-message=""
-          data-color="#FF813F"
-          data-position="Right"
-          data-x_margin="18"
-          data-y_margin="18"
-        ></script>
         <body>
           <Main />
           <NextScript />
