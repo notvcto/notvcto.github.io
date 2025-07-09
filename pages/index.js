fb2483c2a89d248e8ef09a98b12820749c9b4b21
notvcto <git@notvc.to>
2025-07-09 18:06:38 -0400

    Fix Google Analytics tracking ID error

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
+
+// Only initialize GA if we have a valid tracking ID
+if (TRACKING_ID && TRACKING_ID !== 'G-XXXXXXXXXX') {
+  ReactGA.initialize(TRACKING_ID);
+}
 
 function App() {
   return (
