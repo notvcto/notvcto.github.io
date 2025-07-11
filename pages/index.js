ae7524bf48b7c4a8fe86f9184db1e1b5134be314
vcto_ <82398490+notvcto@users.noreply.github.com>
2025-07-11 14:22:47 -0400

    formatting and some slight changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Ubuntu from "../components/ubuntu";
-import ReactGA from 'react-ga4';
-import { useEffect } from 'react';
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 import Meta from "../components/SEO/Meta";
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
@@ -8,7 +8,7 @@ const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
 function App() {
   useEffect(() => {
     // Only initialize GA if we have a valid tracking ID
-    if (TRACKING_ID && TRACKING_ID !== 'G-XXXXXXXXXX') {
+    if (TRACKING_ID && TRACKING_ID !== "G-XXXXXXXXXX") {
       ReactGA.initialize(TRACKING_ID);
     }
   }, []);
@@ -18,7 +18,7 @@ function App() {
       <Meta />
       <Ubuntu />
     </>
-  )
+  );
 }
 
 export default App;
