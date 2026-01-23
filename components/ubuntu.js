431e7352ae72fe01be81eb717b34cf06582025da
notvcto <git@notvc.to>
2026-01-23 21:26:11 +0000

    Hardcode EmailJS creds, update Contact icon & wallpaper

--- components/ubuntu.js
+++ components/ubuntu.js
@@ -10,7 +10,7 @@ export default class Ubuntu extends Component {
     super();
     this.state = {
       screen_locked: false,
-      bg_image_name: "minimal",
+      bg_image_name: "wall-2",
       booting_screen: true,
       shutDownScreen: false,
       accent_color: "#E95420",
