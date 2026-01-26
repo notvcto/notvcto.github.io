10e97494b3e521bae2739eed0f90257b017abd64
notvcto <git@notvc.to>
2026-01-26 01:09:27 +0000

    Redesign Blog App UI to match Achievements layout

--- components/blog/utils.js
+++ components/blog/utils.js
@@ -1,4 +1,4 @@
-export const getPostIcon = (tags: string[] = []): string => {
+export const getPostIcon = (tags = []) => {
   if (!tags) return 'gedit';
   if (tags.includes('terminal')) return 'terminal';
   if (tags.includes('vscode') || tags.includes('code')) return 'vscode';
@@ -7,5 +7,6 @@ export const getPostIcon = (tags: string[] = []): string => {
   if (tags.includes('calc')) return 'calc';
   if (tags.includes('settings')) return 'settings';
   if (tags.includes('firefox') || tags.includes('browser')) return 'firefox';
+  if (tags.includes('hackerone')) return 'hackerone';
   return 'gedit';
 };
